208964809bfbc00ad815bb22e0f6b33736761727
vaishgithubvdj <vaishjambhule@gmail.com>
2023-11-05 15:18:46 +0530

    commit changes

---
+++ src/pack1/class1.java
@@ -0,0 +1,13 @@
+package pack1;
+
+public class class1 {
+	
+	public static void main(String[] args)   {
+		
+		int a = 78;
+		int b = 22;
+		
+		System.out.println(a+b);
+		System.out.println(b);
+	}
+}
