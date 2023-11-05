0ffc0392c94bdbc00739600eb1b6279783354e4e
vaishgithubvdj <vaishjambhule@gmail.com>
2023-11-05 15:35:10 +0530

    commit packages

---
+++ src/addpackmastervdj1/class1.java
@@ -0,0 +1,13 @@
+package addpackmastervdj1;
+
+public class class1 {
+	
+	public static void main(String[] args)   {
+		
+		int a = 89;
+		int b = 123;
+		
+		System.out.println(a-b);
+		System.out.println(a*b);
+	}
+}
